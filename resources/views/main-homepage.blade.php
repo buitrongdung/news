e904fb10d370928ee0c7e20864f8b3920cde7f54
Dzung <dung.bui@whl.travel>
2017-10-26 16:10:48 +0700

    aa

---
+++ resources/views/main-homepage.blade.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html><!--[if lt IE 9]>
+<html class='lt-ie9 no-js' lang='en'>
+<![endif]-->
+<!--[if gte IE 9]><!-->
+<html class="no-js" lang="en">
+<!--<![endif]-->
+<html dir="ltr" lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="msapplication-tap-highlight" content="no">
+    <meta name="theme-color" content="#005aab">
+    <title>Wedding Restaurant</title>
+    <link rel="shortcut icon" href="img/favicon.png">
+    <link rel="stylesheet" href="https://cdn.linearicons.com/free/1.0.0/icon-font.min.css">
+    <!-- inject:js -->
+    <script src="{{asset('js/common/addons.js')}}"></script>
+    <script src="{{asset('js/common/main.js')}}"></script>
+    <!-- endinject -->
+    <!-- inject:css -->
+    <link href="{{asset('/css/addons.css')}}" rel="stylesheet">
+    <link href="{{asset('/css/main.css')}}" rel="stylesheet">
+    <!-- endinject -->
+  </head>
+  <body class="homepage">
+    <div class="fullpage">
+      <header>
+        <section class="headertop clearfix">
+          <div class="container">
+            <div class="topinfo clearfix">
+              <div class="hotline"><a href="tel:(234) 567-891"><i class="fa fa-phone" aria-hidden="true"></i> 1 (234) 567-891</a></div>
+              <div class="email"><a href="mailto:nfo@grandrestaurant.com.vn"><i class="fa fa-envelope" aria-hidden="true"></i> nfo@grandrestaurant.com.vn</a></div>
+              <time><i class="fa fa-clock-o" aria-hidden="true"></i> Monday – Sunday 10 AM – 10 PM</time>
+            </div>
+            <div class="social clearfix"><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a><a href="#"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></div>
+          </div>
+        </section>
+        <section class="headermain clearfix">
+          <div class="container">
+            <div class="btn-showmenu"><span></span></div>
+            <div class="overlay"></div>
+            <div class="sitelogo"><a href="index.html"><img src="img/logo.png" alt=""></a></div>
+            <div class="searchtoggle hidden-lg-up"><i class="fa fa-search" aria-hidden="true"></i></div>
+            <nav class="menu clearfix">
+              <ul class="menulink clearfix">
+                <li class="active"><a href="/">Trang chủ</a></li>
+                <li><a href="/about">Giới thiệu</a></li>
+                <li><a href="dichvu.html">Dịch vụ</a></li>
+                <li><a href="datban.html">Đặt bàn</a></li>
+                <li><a href="thucdon.html">Thực đơn</a></li>
+                <li><a href="lienhe.html">Liên hệ</a></li>
+                <li><a href="dangnhap.html">Đăng nhập</a></li>
+              </ul>
+              <div class="searchtoggle hidden-md-down"><i class="fa fa-search" aria-hidden="true"></i></div>
+            </nav>
+            <div class="search clearfix">
+              <div class="btn-closesearch"><span class="lnr lnr-cross"></span></div>
+              <div class="searchwrap clearfix">
+                <input type="text" name="search_engine" id="search_engine" placeholder="Tìm kiếm...">
+                <button class="searchbutton"><i class="fa fa-search" aria-hidden="true"></i></button>
+              </div>
+            </div>
+          </div>
+        </section>
+      </header>
+      <section class="bannerwrap clearfix">
+        <div id="bannerwrap" class="rev_slider_wrapper fullscreen-container">
+          <div id="banner001" data-version="5.1.1RC" class="rev_slider fullscreenbanner">
+            <ul>
+              <li data-transition="random"><img src="img/banner/6.jpg" alt="" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" data-bgparallax="5" data-no-retina>
+              </li>
+              <li data-transition="random"><img src="img/banner/7.jpg" alt="" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" data-bgparallax="5" data-no-retina>
+              </li>
+              <li data-transition="random"><img src="img/banner/8.jpg" alt="" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" data-bgparallax="5" data-no-retina>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+      <main>
+        <section class="home-about clearfix">
+          <div class="container">
+            <h1 class="headtitle">Wedding & Convention</h1>
+            <div class="about-content">
+              <figure>
+                <div class="img"><img src="img/about/1.jpg" alt=""></div>
+                <figcaption>
+                  <div class="pagetitle">Chúng tôi là ai?</div>
+                  <div class="content">
+                    <p>Tọa lạc trên trục đường chính cách trung tâm thành phố Hồ Chí Minh khoảng 20 phút lái xe và cách sân bay Tân Sơn Nhất 1.5 km – nằm trong vòng an ninh. Grand Restaurant sở hữu 6 sảnh lớn phối hợp hoàn hảo cùng kiến trúc Châu Âu sang trọng. Hệ thống trang thiết bị hiện đại cùng khả năng thay đổi thiết kế linh hoạt với sức chứa lên đến hơn 2.500 khách, Grand Restaurant là địa điểm lý tưởng để tổ chức hội nghị và yến tiệc. </p>
+                    <p>Đến với Grand Restaurant, bạn sẽ được trải nghiệm những tiện nghi và dịch vụ tuyệt vời của một trong những Trung tâm Hội nghị - Tiệc cưới uy tín hàng đầu trong khu vực thành phố Hồ Chí Minh.</p>
+                  </div><a href="gioithieu.html" class="hvr-shutter-out-horizontal link">Xem thêm</a>
+                </figcaption>
+              </figure>
+            </div>
+          </div>
+        </section>
+      </main>
+      <footer class="text-xs-center">
+        <div class="container">
+          <div class="footertop clearfix">
+            <nav class="footernav clearfix">
+              <ul>
+                <li><a href="index.html">Trang chủ</a></li>
+                <li><a href="gioithieu.html">Giới thiệu</a></li>
+                <li><a href="dichvu.html">Dịch vụ</a></li>
+                <li><a href="datban.html">Đặt bàn</a></li>
+                <li><a href="thucdon.html">Thực đơn</a></li>
+                <li><a href="lienhe.html">Liên hệ</a></li>
+                <li><a href="dangnhap.html">Đăng nhập</a></li>
+              </ul>
+            </nav>
+            <div class="social clearfix"><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a><a href="#"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></div>
+            <div class="contactinfo clearfix">
+              <div class="company">TRUNG TÂM HỘI NGHỊ & TIỆC CƯỚI Grand Restaurant</div>
+              <ul>
+                <li><i class="fa fa-map-marker" aria-hidden="true"></i> 12 Nguyễn Văn Bảo, P.4, Q.Gò Vấp, TP.Hồ Chí Minh</li>
+                <li><i class="fa fa-phone" aria-hidden="true"></i> Điện thoại: (08) 3811 8181 - <i class="fa fa-envelope" aria-hidden="true"></i> Email: info@grandrestaurant.com.vn</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="footerbottom clearfix">
+          <div class="container">
+            <div class="copyright">Copyright © 2017 Grand Restaurant. All rights reserved.</div>
+          </div>
+        </div>
+      </footer>
+      <div class="up"><a href="#"><i class="fa fa-angle-up" aria-hidden="true" style="color: white;"></i></a></div>
+    </div>
+  </body>
+</html></html>
